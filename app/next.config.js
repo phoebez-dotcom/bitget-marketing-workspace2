710e59ac92c30aa18d69f859a8cb4aca8e080973
phoebez-dotcom <phoebe.z@bitget.com>
2026-05-26 17:42:35 +0800

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,13 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // 💥 终极放行：允许 Vercel 打包时完全忽略 TypeScript 的类型报错
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // 💥 终极放行：允许 Vercel 打包时完全忽略 ESLint 的语法规范报错
-    ignoreDuringBuilds: true,
-  },
-};
 
-module.exports = nextConfig;
